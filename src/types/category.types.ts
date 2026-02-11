de08c04babd723fb45c435952434c3742554aa83
Salman Farsi <salmanfarsi9005@gmail.com>
2026-02-11 22:52:46 +0600

    feat: update types of Season

--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -1,5 +1,5 @@
 export type Gender = "MEN" | "WOMEN" | "UNISEX";
-export type Season = "SUMMER" | "WINTER" | "ALL";
+export type Season = "SUMMER" | "WINTER" | "ALL SEASONS";
 
 export interface ICategory {
   name: string;
